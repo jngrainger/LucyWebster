ad3ae0838dc40ceadc62f265c99e70d6f8d46dfd
elliotcw <williams.elliot@gmail.com>
2018-04-16 23:02:33 +0100

    overlay

--- src/Overview/Overview.js
+++ src/Overview/Overview.js
@@ -1,39 +1,52 @@
 import React from 'react';
 import chunk from 'lodash/chunk';
+import flatten from 'lodash/flatten';
 import collections from './collections';
 import Slideshow from '../Slideshow';
 import FlickType from '../FlickType';
 
 export default class Overview extends React.Component {
-  
   render() {
-    return chunk(collections, 2).map(pair => (
-      <div style={{ display: 'flex', flexDirection: 'row', width: '100%', paddingTop: 240, justifyContent: 'stretch' }}>
-        {pair[0] && (
-          <div style={{ width: '50%', minWidth: 350 }}>
-            <Slideshow
-              jump
-              collectionInformation={pair[0]}
-              numberInRotation={3}
-              autoFlick={FlickType.EVEN}
-              showTitle={false}
-              showDescription={false}
-            />
-          </div>
-        )}
-        {pair[1] && (
-          <div style={{ width: '50%', alignSelf: 'flex-end' }}>
-            <Slideshow
-              jump
-              collectionInformation={pair[1]}
-              numberInRotation={3}
-              autoFlick={FlickType.ODD}
-              showTitle={false}
-              showDescription={false}
-            />
-          </div>
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          width: '100%',
+          flexWrap: 'wrap',
+          paddingTop: 240,
+          justifyContent: 'stretch',
+        }}
+      >
+        {flatten(
+          chunk(collections, 2).map(pair => [
+            pair[0] ? (
+              <div style={{ flex: '1 1 50%', minWidth: 150 }}>
+                <Slideshow
+                  jump
+                  collectionInformation={pair[0]}
+                  numberInRotation={3}
+                  autoFlick={FlickType.EVEN}
+                  showTitle={false}
+                  showDescription={false}
+                />
+              </div>
+            ) : null,
+            pair[1] ? (
+              <div style={{ flex: '1 1 50%', alignSelf: 'flex-end', paddingTop: 360 }}>
+                <Slideshow
+                  jump
+                  collectionInformation={pair[1]}
+                  numberInRotation={3}
+                  autoFlick={FlickType.ODD}
+                  showTitle={false}
+                  showDescription={false}
+                />
+              </div>
+            ) : null,
+          ])
         )}
       </div>
-    ));
+    );
   }
 }
